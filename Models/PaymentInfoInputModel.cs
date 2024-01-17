0188111156ba32e77bdcd8fa0317ecad9e568f37
Carlos Henrique <carlosmvsep3@hotmail.com>
2024-01-16 23:15:51 -0300

    arrumando o nome do expiresAt no modelo de dominio do pagamento

--- Models/PaymentInfoInputModel.cs
+++ Models/PaymentInfoInputModel.cs
@@ -5,7 +5,7 @@
         public int IdProject { get; set; }
         public string CreditCardNumber { get; set; }
         public string Cvv { get; set; }
-        public string ExpiresAy { get; set; }
+        public string ExpiresAt { get; set; }
         public string FullName { get; set; }
         public decimal Amount { get; set; }
     }
